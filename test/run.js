698b375a124acd7c083a1dea7d87afb98d9eec36
Quildreen Motta <quildreen@gmail.com>
2014-10-04 23:03:21 -0300

    Updates libraries.

--- test/run.js
+++ test/run.js
@@ -22,9 +22,3 @@
 var hifive = require('hifive')
 
 hifive.runWithDefaults(require('./specs'), require('hifive-spec')())
-      .fork( function(e) { throw e }
-           , function(report) {
-               if (report.failed.length)  process.exit(1)
-               if (!report.all().length)  process.exit(1)
-               else                       process.exit(0)
-             })
